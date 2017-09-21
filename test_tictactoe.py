80494a177154355a5c8db99558ccfd799ae1382b
Linn <lilo4591@student.uu.se>
2017-09-21 12:49:59 +0200

    added tests for userinputfunctions and some gamestatesfunc

--- test_tictactoe.py
+++ test_tictactoe.py
@@ -1,9 +1,13 @@
-import unittest
+import unittest, mock, io
+from contextlib import redirect_stdout
 import tactoe
 
+# Uppsala University
+# Software Engineering and Project Management autumn 2017
+# Group L
+# Author Linn Löfquist
+
 #TODO:
-# 1. Mainloop in tictactoe.py needs to be moved to another file
-#    so the automated tests can be run.
 # 
 # 2. Depending om indexing design decisions, change testGameWon to reflect that
 #
@@ -12,12 +16,15 @@ import tactoe
 #    [a,s,d] and [g,h,j] mapping to [4,5,6] or [3,4,5] depeding on designdecision
 #    [z,x,c]     [b,n,m]            [7,8,9]    [6,7,8]
 # 
-# 4. Implement test markers left
-# 5. Implement test timeout
 # 6. Implement test faulty input
-# 7. Since current version of code to test is python 2.something, tests are accordingly
-#    update teststuff to 3.6
 # 8. moar stuff
+#
+# update!
+# 
+# functions tested isGameWon, playAgain,GetPlayerNames, isGamestatefull, performMove
+# functions left to test getPlayerMove(io), isPositionFree, getGameMove
+# functions not tested printGameState, printMoves (printfunction no need to test)
+
 
 #lists are of 10 elements but index 0 never used, ' ' for first element in the list. 
 #tests all possible ways of winning the game
@@ -72,6 +79,93 @@ class testGameWon(unittest.TestCase):
         gameStateDrawn = [' ', 'X', 'Y', 'X', 'Y', 'X', 'X', 'Y', 'X', 'Y']
         self.assertEqual(tactoe.isGameWon(gameStateDrawn, 'Y'), False)
         self.assertEqual(tactoe.isGameWon(gameStateDrawn, 'X'), False)
+
+#tests all userinput functions
+class testUserInputs(unittest.TestCase):
+ 
+    #mocking a userinput
+    @mock.patch('builtins.input', side_effect='n')
+    def testPlayAgainYes(self, input):
+        a = io.StringIO()
+        #redirects the the output from playagain function
+        with redirect_stdout(a):
+            ans = tactoe.playAgain()
+        self.assertEqual(ans, False)
+
+    @mock.patch('builtins.input', side_effect='y')
+    def testPlayAgainNo(self, input):
+        a = io.StringIO()
+        #redirects the the output from playagain function
+        with redirect_stdout(a):
+            ans = tactoe.playAgain()
+        self.assertEqual(ans, True)
+
+    @mock.patch('builtins.input', side_effect= ['Orjan', 'Lisa'])
+    def testGetPlayerNames1(self, input):
+        playernames = []
+        a = io.StringIO()
+        #redirects the the output from playagain function
+        with redirect_stdout(a):
+            actualplayers = tactoe.getPlayerNames('0')    
+        self.assertEqual(actualplayers, ['Orjan', 'Lisa'])
+        self.assertTrue(len(actualplayers) == 2)
+
+    @mock.patch('builtins.input', side_effect=['examplename'])
+    def testGetPlayerNames1(self, input):
+        playernames = []
+        a = io.StringIO()
+        #redirects the the output from playagain function
+        with redirect_stdout(a):
+            actualplayers = tactoe.getPlayerNames('1')    
+        self.assertEqual(actualplayers[0], 'examplename')
+        self.assertTrue(len(actualplayers) == 2)
+
+#tests all gamealtering functions
+class testGameActions(unittest.TestCase):
+
+    def testisGameStateFull(self):
+        #when gamestate is full
+        gamestate = ['X','X','O','X','O','X','O','X','O','X']
+        self.assertTrue(tactoe.isGameStateFull(gamestate)) 
+        gamestate = [' ','X','O','X','O','X','O','X','O','X']
+        self.assertTrue(tactoe.isGameStateFull(gamestate))
+        
+        #when gamestate is not full
+        gamestate = [' ']*10
+        self.assertFalse(tactoe.isGameStateFull(gamestate))
+        gamestate = [' ',' ','O','X','O','X','O','X','O','X']
+        self.assertFalse(tactoe.isGameStateFull(gamestate))
+        gamestate = [' ',' ','O','X','O','X','O',' ','O','X']
+        self.assertFalse(tactoe.isGameStateFull(gamestate))
+
+    def testperformMove(self):
+        gameState = [' ']*10
+        marker = 'X'
+        movesLeft = [['X','X','X','X','X'],['O','O','O','O']]
+        size = 5 # X always starts with 5 stones
+        iterate = 1
+
+        while movesLeft[0] != []:
+            #every time a move is placed,check location of placement in gamestate
+            #and check number markers left in movesleft
+            tactoe.performMove(gameState, marker, iterate, movesLeft)
+            size = size - 1
+            self.assertEqual(len(movesLeft[0]),size)
+            self.assertEqual(gameState[iterate], 'X')
+            iterate = iterate + 1
+
+        gameState = [' ']*10
+        marker = 'Y'
+        size = 4 #Y always starts with 4 stones
+        iterate = 1
+
+        while movesLeft[1] != []:
+            tactoe.performMove(gameState, marker, iterate, movesLeft)
+            size = size - 1
+            self.assertEqual(len(movesLeft[1]),size)
+            self.assertEqual(gameState[iterate], 'Y')
+            iterate = iterate + 1
+
 
 if __name__ == '__main__':
     unittest.main()
